a467cbc5b69752d5d479b04cab83bd4317506b8d
Tamil Kumaran R <159413206+TK265@users.noreply.github.com>
2024-07-12 08:54:49 +0530

    Add files via upload

---
+++ src/components/styledComponents/quizButtons.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Button = styled.button `
+  height: 120px;
+  width: 120px;
+  margin: 15px;
+
+  border: 5px inset grey;
+  border-radius: 10px;
+
+  font-family: Cabin;
+  font-size: 15px;
+  font-weight: 500;
+
+  display: inline-grid;
+  vertical-align: top;
+  justify-content: space-evenly;
+`;
+
+export default Button;
---
+++ src/components/styledComponents/title.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.text `
+  font-size: 120px;
+  font-weight: 500;
+  font-family: Righteous;
+`;
+
+export default Title;
+
+// text-shadow: -2px 0 darkgrey, 0 5px black, 4px 0 white, 0 -1px grey;
